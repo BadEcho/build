25d5591229e78ac402892a308e9f37647b800359
Matthew Weber <Matthew.Weber@toro.com>
2023-08-22 12:35:00 -0500

    Adding C++ project support
    MSBuild equivalents are used in place of dotnet.
    
    New parameter is also added to specify configuration used with Pack.
    
    Needed so we don't try to pack any C++ projects.

--- build.ps1
+++ build.ps1
@@ -3,6 +3,8 @@
 param (
 	[string]$CommitId,
 	[string]$VersionDistance,
+	# Used to specify the configuration build to use in place of Release to prevent attempts to package native projects.
+	[string]$PackageConfiguration,
 	[switch]$SkipTests
 )
 
@@ -17,8 +19,12 @@ function AppendCommand([string]$command, [string]$commandSuffix){
 	return [ScriptBlock]::Create($command + $commandSuffix)
 }
 
+if (!$PackageConfiguration) {
+	$PackageConfiguration = "Release"
+}
+
 New-Item -ItemType Directory -Force .\artifacts
-$artifacts = Resolve-Path .\artifacts\ | select -ExpandProperty Path
+$artifacts = Resolve-Path .\artifacts\ | Select-Object -ExpandProperty Path
 
 if (Test-Path $artifacts) {
 	Remove-Item $artifacts -Force -Recurse
@@ -29,23 +35,25 @@ $majorVersion = $versionSettings[0].majorVersion
 $minorVersion = $versionSettings[0].minorVersion
 $patchVersion = $versionSettings[0].patchVersion
 
-$buildCommand =  { & dotnet build -c Release -p:MajorVersion=$majorVersion -p:MinorVersion=$minorVersion -p:PatchVersion=$patchVersion }
-$packCommand = { & dotnet pack -c Release -p:PackageOutputPath=$artifacts --no-build -p:MajorVersion=$majorVersion -p:MinorVersion=$minorVersion -p:PatchVersion=$patchVersion }
+$buildCommand =  { & msbuild -p:Configuration=Release -p:MajorVersion=$majorVersion -p:MinorVersion=$minorVersion -p:PatchVersion=$patchVersion }
+# If there are any native projects in the solution, then a separate configuration created specifically for use during NuGet package creation needs to be made.
+# This configuration needs to have all native projects excluded from being built so we don't attempt to pack them. Normally, we'd assign a value of false to the
+# IsPackable element for the project, however MSBuild ignores this property and errors out anyway.
+$packCommand = { & msbuild -t:Pack -p:Configuration=$PackageConfiguration -p:PackageOutputPath=$artifacts -p:NoBuild=true -p:MajorVersion=$majorVersion -p:MinorVersion=$minorVersion -p:PatchVersion=$patchVersion }
 
 if($CommitId -and $VersionDistance) {	
-	$prereleaseId = $versionSettings[0].prereleaseId
-		
+	$prereleaseId = $versionSettings[0].prereleaseId		
 	$versionCommand = "-p:BuildMetadata=$CommitId -p:PrereleaseId=$prereleaseId -p:BuildNumber=$VersionDistance"
 
 	$buildCommand = AppendCommand($buildCommand.ToString(), $versionCommand)
 	$packCommand = AppendCommand($packCommand.ToString(), $versionCommand)
 }
 
-Execute { & dotnet clean -c Release }
+Execute { & msbuild -p:Configuration=Release -t:Clean }
 Execute $buildCommand 
 
-if ($SkipTests -ne $true) {
-	Execute { & dotnet test -c Release --results-directory $artifacts --no-build -l trx --verbosity=normal }
+if ($SkipTests -ne $true) {	
+	Execute { & vstest.console /Logger:trx /ResultsDirectory:$artifacts (Get-ChildItem bin\rel\*.Tests.dll).FullName }
 }
 
 Execute $packCommand

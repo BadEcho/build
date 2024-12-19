cceea5e43d21b321692ed80d10293a60524e4d7b
Matt Weber <matt@badecho.com>
2024-12-19 04:25:43 -0600

    Script to get conclusion of workflow run

---
+++ Get-LatestWorkflowConclusion.ps1
@@ -0,0 +1,19 @@
+# Gets the result of the most recent run for a workflow.
+
+param (
+    [string]$Owner,
+    [string]$Repository,
+    [string]$Branch,
+    [string]$WorkflowPath
+)
+
+$conclusion = $null
+
+$allRuns = gh api "/repos/$Owner/$Repository/actions/runs" | ConvertFrom-Json
+$workflowRuns = $allRuns.workflow_runs.Where({$_.path -eq $WorkflowPath -and $_.head_branch -eq $Branch})
+
+if ($workflowRuns) {
+    $conclusion = $workflowRuns[0].conclusion
+}
+
+return $conclusion

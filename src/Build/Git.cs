432bad34301ee37c0451312355f245c8d74213d4
Matt Weber <matt@BadEcho.com>
2026-03-25 15:02:43 -0500

    Revert "Set Build task framework to netstandard2.1"
    This reverts commit 5755518ff0b3befd5c718878d71deeb8048fbedc.

--- src/Build/Git.cs
+++ src/Build/Git.cs
@@ -15,77 +15,76 @@ using System.Text;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
-namespace BadEcho.Build
+namespace BadEcho.Build;
+
+/// <summary>
+/// Provides a task that will execute a Git command.
+/// </summary>
+public sealed class Git : ToolTask
 {
+    private readonly StringBuilder _standardOutput = new();
+
     /// <summary>
-    /// Provides a task that will execute a Git command.
+    /// Gets or sets the Git command to execute.
     /// </summary>
-    public sealed class Git : ToolTask
+    [Required]
+    public string? Command
+    { get; set; }
+
+    /// <summary>
+    /// Gets or sets arguments to pass to the Git command.
+    /// </summary>
+    public string? Arguments
+    { get; set; }
+
+    /// <summary>
+    /// Gets the standard output of the Git command.
+    /// </summary>
+    [Output]
+    public string? Output
+    { get; private set; }
+
+    /// <inheritdoc/>
+    protected override string ToolName
+        => "git.exe";
+
+    /// <inheritdoc/>
+    public override bool Execute()
     {
-        private readonly StringBuilder _standardOutput = new StringBuilder();
+        _ = ExecuteTool(GenerateFullPathToTool(), string.Empty, GenerateCommandLineCommands());
 
-        /// <summary>
-        /// Gets or sets the Git command to execute.
-        /// </summary>
-        [Required]
-        public string? Command
-        { get; set; }
-
-        /// <summary>
-        /// Gets or sets arguments to pass to the Git command.
-        /// </summary>
-        public string? Arguments
-        { get; set; }
-
-        /// <summary>
-        /// Gets the standard output of the Git command.
-        /// </summary>
-        [Output]
-        public string? Output
-        { get; private set; }
-
-        /// <inheritdoc/>
-        protected override string ToolName
-            => "git.exe";
-
-        /// <inheritdoc/>
-        public override bool Execute()
+        if (!Log.HasLoggedErrors)
         {
-            _ = ExecuteTool(GenerateFullPathToTool(), string.Empty, GenerateCommandLineCommands());
-
-            if (!Log.HasLoggedErrors)
-            {
-                Output = _standardOutput.ToString().Trim();
-            }
+            Output = _standardOutput.ToString().Trim();
+        }
         
-            return !Log.HasLoggedErrors;
-        }
+        return !Log.HasLoggedErrors;
+    }
 
-        /// <inheritdoc/>
-        protected override string GenerateFullPathToTool()
-            // The runtime will automatically search the system path for us.
-            => ToolName;
+    /// <inheritdoc/>
+    protected override string GenerateFullPathToTool()
+        // The runtime will automatically search the system path for us.
+        => ToolName;
 
-        /// <inheritdoc/>
-        protected override string GenerateCommandLineCommands()
-        {
-            var commandLine = new CommandLineBuilder();
+    /// <inheritdoc/>
+    protected override string GenerateCommandLineCommands()
+    {
+        var commandLine = new CommandLineBuilder();
 
-            commandLine.AppendTextUnquoted(Command);
+        commandLine.AppendTextUnquoted(Command);
 
-            if (!string.IsNullOrEmpty(Arguments))
-                commandLine.AppendSwitch(Arguments);
+        if (!string.IsNullOrEmpty(Arguments))
+            commandLine.AppendSwitch(Arguments);
 
-            return commandLine.ToString();
-        }
+        return commandLine.ToString();
+    }
 
-        /// <inheritdoc/>
-        protected override void LogEventsFromTextOutput(string singleLine, MessageImportance messageImportance)
-        {
-            if (!string.IsNullOrEmpty(singleLine))
-                _ = _standardOutput.AppendLine(singleLine);
+    /// <inheritdoc/>
+    protected override void LogEventsFromTextOutput(string singleLine, MessageImportance messageImportance)
+    {
+        if (!string.IsNullOrEmpty(singleLine))
+            _ = _standardOutput.AppendLine(singleLine);
 
-            base.LogEventsFromTextOutput(singleLine, messageImportance);
-        }
+        base.LogEventsFromTextOutput(singleLine, messageImportance);
     }
 }
--- src/Build/ReadJsonFile.cs
+++ src/Build/ReadJsonFile.cs
@@ -11,9 +11,6 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using BadEcho.Build.Properties;
@@ -21,71 +18,70 @@ using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using Task = Microsoft.Build.Utilities.Task;
 
-namespace BadEcho.Build
+namespace BadEcho.Build;
+
+/// <summary>
+/// Provides an MSBuild task that will parse a JSON file and return metadata containing the contents of said file.
+/// </summary>
+public sealed class ReadJsonFile : Task
 {
     /// <summary>
-    /// Provides an MSBuild task that will parse a JSON file and return metadata containing the contents of said file.
+    /// Gets or sets the path to the JSON file to parse.
     /// </summary>
-    public sealed class ReadJsonFile : Task
+    [Required]
+    public string? Path 
+    { get; set; }
+
+    /// <summary>
+    /// Gets the resulting task item whose metadata has been loaded with the JSON file's properties.
+    /// </summary>
+    [Output]
+    public ITaskItem? Output 
+    { get; private set; }
+
+    /// <inheritdoc/>
+    public override bool Execute()
     {
-        /// <summary>
-        /// Gets or sets the path to the JSON file to parse.
-        /// </summary>
-        [Required]
-        public string? Path 
-        { get; set; }
-
-        /// <summary>
-        /// Gets the resulting task item whose metadata has been loaded with the JSON file's properties.
-        /// </summary>
-        [Output]
-        public ITaskItem? Output 
-        { get; private set; }
-
-        /// <inheritdoc/>
-        public override bool Execute()
+        if (string.IsNullOrEmpty(Path))
         {
-            if (string.IsNullOrEmpty(Path))
-            {
-                Log.LogErrorFromResources(nameof(Strings.NoPathProvided));
-                return false;
-            }
-
-            JsonNode? content = JsonNode.Parse(File.ReadAllText(Path));
-
-            if (content == null)
-            {
-                Log.LogErrorFromResources(nameof(Strings.JsonParseFailed), Path);
-                return false;
-            }
-
-            JsonObject contentObject = content.AsObject();
-
-            var taskItem = new TaskItem(contentObject.ToJsonString());
-
-            AddPropertiesToItem(contentObject, taskItem);
-
-            Output = taskItem;
-            return true;
+            Log.LogErrorFromResources(nameof(Strings.NoPathProvided));
+            return false;
         }
 
-        private static void AddPropertiesToItem(JsonObject jsonObject, TaskItem taskItem, string rootName = "")
+        JsonNode? content = JsonNode.Parse(File.ReadAllText(Path));
+
+        if (content == null)
         {
-            IEnumerable<string> propertyNames = jsonObject.Select(kv => kv.Key);
+            Log.LogErrorFromResources(nameof(Strings.JsonParseFailed), Path);
+            return false;
+        }
 
-            foreach (string propertyName in propertyNames)
+        JsonObject contentObject = content.AsObject();
+
+        var taskItem = new TaskItem(contentObject.ToJsonString());
+
+        AddPropertiesToItem(contentObject, taskItem);
+
+        Output = taskItem;
+        return true;
+    }
+
+    private static void AddPropertiesToItem(JsonObject jsonObject, TaskItem taskItem, string rootName = "")
+    {
+        IEnumerable<string> propertyNames = jsonObject.Select(kv => kv.Key);
+
+        foreach (string propertyName in propertyNames)
+        {
+            JsonNode? property = jsonObject[propertyName];
+
+            if (property?.GetValueKind() == JsonValueKind.Object)
             {
-                JsonNode? property = jsonObject[propertyName];
-
-                if (property?.GetValueKind() == JsonValueKind.Object)
-                {
-                    JsonObject propertyObject = property.AsObject();
-                    AddPropertiesToItem(propertyObject, taskItem, $"{rootName}{propertyName}");
-                }
-                else
-                {
-                    taskItem.SetMetadata($"{rootName}{propertyName}", property?.GetValue<object>().ToString());
-                }
+                JsonObject propertyObject = property.AsObject();
+                AddPropertiesToItem(propertyObject, taskItem, $"{rootName}{propertyName}");
+            }
+            else
+            {
+                taskItem.SetMetadata($"{rootName}{propertyName}", property?.GetValue<object>().ToString());
             }
         }
     }
